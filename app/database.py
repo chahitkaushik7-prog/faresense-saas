a9cf42963c3bcbf1f711330cf39a5bf86bb6357c
Chahit Kaushik <chahitkaushik7@gmail.com>
2026-02-27 13:17:57 +0530

    Fixed database engine properly

--- app/database.py
+++ app/database.py
@@ -4,9 +4,12 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine("postgresql://postgres:GGcljqMuIGDUsUFUyFOhldaefplunoTh@postgres.railway.internal:5432/railway")
-SessionLocal = sessionmaker(bind=engine)
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set")
+
 engine = create_engine(
     DATABASE_URL,
     connect_args={"sslmode": "require"}
 )
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
